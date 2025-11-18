46002d01f63397a0a94aa36d92c8d9c6c60a2485
Muhammad Faheem <malikfaheem.007001@gmail.com>
2025-11-18 18:45:10 +0500

    data fetch file

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,19 +1,56 @@
 import {Product} from "@/sanity.types";
 import {urlFor} from "@/sanity/lib/image";
+import {Flame} from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import AddToWishlistButton from "./AddToWishlistButton";
+import {Title} from "./ui/text";
 
 const ProductCard = ({product}: {product: Product}) => {
   return (
-    <div>
-      {product?.images && (
-        <Image
-          src={urlFor(product?.images[0]).url()}
-          alt="ProductImage"
-          loading="lazy"
-          width={700}
-          height={700}
-        />
-      )}
+    <div className="text-sm border-[1px] border-dark_blue/20 rounded-md bg-white group">
+      <div className="relative group overflow-hidden bg-shop_light_bg">
+        {product?.images && (
+          <Image
+            src={urlFor(product?.images[0]).url()}
+            alt="ProductImage"
+            loading="lazy"
+            width={700}
+            height={700}
+            className={`w-full h-64 object-contain overflow-hidden transition-transform bg-shop_light_bg hoverEffect ${product?.stock !== 0 ? "group-hover:scale-105" : "opacity-50"}`}
+          />
+        )}
+        <AddToWishlistButton product={product} />
+        {product?.status === "sale" && (
+          <p className="absolute top-2 left-2 z-10 text-xs border border-darkColor/50 px-2 rounded-full group-hover:border-shop_light_green group-hover:text-shop_light_green hoverEffect">
+            Sale!
+          </p>
+        )}
+        {product?.status === "new" && (
+          <p className="absolute top-2 left-2 z-10 text-xs border border-darkColor/50 px-2 rounded-full group-hover:border-shop_light_green group-hover:text-shop_light_green hoverEffect">
+            New!
+          </p>
+        )}
+        {product?.status === "hot" && (
+          <Link
+            href={"/deal"}
+            className="absolute top-2 left-2 z-10 border border-shop_orange/50 p-1 rounded-full group-hover:border-shop_orange hover:text-shop_dark_green hoverEffect">
+            <Flame
+              size={18}
+              fill="#fb6c08"
+              className="text-shop_orange/50 group-hover:text-shop_orange hoverEffect"
+            />
+          </Link>
+        )}
+      </div>
+      <div className="py-3 flex flex-col gap-2">
+        {product?.categories && (
+          <p className="uppercase line-clamp-1 text-xs text-shop_light_text">
+            {product?.categories.map((cat) => cat).join(",  ")}
+          </p>
+        )}
+        <Title className="text-sm line-clamp-1">{product?.name}</Title>
+      </div>
     </div>
   );
 };
--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -45,7 +45,11 @@ const ProductGrid = () => {
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2.5 mt-10">
           {products?.map((product) => (
             <AnimatePresence key={product?._id}>
-              <motion.div>
+              <motion.div
+                layout
+                initial={{opacity: 0.2}}
+                animate={{opacity: 1}}
+                exit={{opacity: 0}}>
                 <ProductCard product={product} />
               </motion.div>
             </AnimatePresence>
