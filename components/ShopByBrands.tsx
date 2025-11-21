c0fc603fc3e76313ed7a7223d0be71974690a3e5
Muhammad Faheem <malikfaheem.007001@gmail.com>
2025-11-21 16:02:52 +0500

    extra data add in file

--- components/ShopByBrands.tsx
+++ components/ShopByBrands.tsx
@@ -3,6 +3,30 @@ import {Title} from "./ui/text";
 import {getAllBrands} from "@/sanity/queries";
 import Image from "next/image";
 import {urlFor} from "@/sanity/lib/image";
+import {GitCompareArrows, Headset, ShieldCheck, Truck} from "lucide-react";
+
+const extraData = [
+  {
+    title: "Free Delivery",
+    description: "Free shipping over $100",
+    icon: <Truck size={45} />,
+  },
+  {
+    title: "Free Return",
+    description: "Free shipping over $100",
+    icon: <GitCompareArrows size={45} />,
+  },
+  {
+    title: "Customer Support",
+    description: "Friendly 24/7 customer support",
+    icon: <Headset size={45} />,
+  },
+  {
+    title: "Money Back Guarantee",
+    description: "Quality checked by our team",
+    icon: <ShieldCheck size={45} />,
+  },
+];
 
 const ShopByBrands = async () => {
   const brands = await getAllBrands();
@@ -32,6 +56,23 @@ const ShopByBrands = async () => {
               />
             )}
           </Link>
+        ))}
+      </div>
+      <div className="grid grid-col-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-16 p-2 shadow-sm hover:shadow-shop_light_green/20 py-5">
+        {extraData?.map((item, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-3 text-lightColor group hover:text-shop_light_green">
+            <span className="inline-flex scale-100 group-hover:scale-90 hoverEffect">
+              {item.icon}
+            </span>
+            <div className="text-sm">
+              <p className="text-darkColor/80 font-bold capitalize">
+                {item.title}
+              </p>
+              <p className="text-lightColor">{item.description}</p>
+            </div>
+          </div>
         ))}
       </div>
     </div>
