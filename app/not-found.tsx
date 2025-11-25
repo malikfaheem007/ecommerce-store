381cf4fcf9bf23fcadfe60fb0992ff69cce36843
Muhammad Faheem <malikfaheem.007001@gmail.com>
2025-11-25 17:20:26 +0500

    not found page add

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,28 +3,28 @@ import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
-    <div className="bg-red-900 flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 py-10 md:py-32">
+    <div className="bg-white flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 py-10 md:py-32">
       <div className="max-w-md w-full space-y-8">
         <div className="text-center">
           <Logo />
           <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
             Looking for something?
           </h2>
-          <p>
-            We&apos;re sorry. The Web address you entered is not functioning
-            page on our site.
+          <p className="mt-2 text-sm text-gray-600">
+            We&apos;re sorry. The Web address you entered is not a functioning
+            page our site.
           </p>
         </div>
         <div className="mt-8 space-y-6">
           <div className="rounded-md shadow-sm space-y-4">
             <Link
-              href="/"
-              className="w-full flex items-center justify-center px-4 py-2 border border-transparent text-sm font-semibold rounded-md text-white bg-shop_dark_green/80 hover:bg-shop_dark_green focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-shop_orange hoverEffect">
-              Go to Shopcart&apos;s Home page
+              href={"/"}
+              className="w-full flex items-center justify-center px-4 py-2 border border-transparent text-sm font-semibold rounded-md text-white bg-shop_dark_green/80 hover:bg-shop_dark_green/90 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-shop_orange hoverEffect">
+              Go to Shopcart&apos;s home page
             </Link>
             <Link
-              href="/help"
-              className="w-full flex items-center justify-center px-4 py-2 border border-gray-300 text-sm font-semibold rounded-md text-blue-400 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400">
+              href={"/help"}
+              className="w-full flex items-center justify-center px-4 py-2 border border-gray-300 text-sm font-semibold rounded-md text-blue-300 bg-white hover:border-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-800 hoverEffect">
               Help
             </Link>
           </div>
@@ -33,15 +33,15 @@ const NotFoundPage = () => {
           <p className="text-sm text-gray-600">
             Need help? Visit the{" "}
             <Link
-              href="/help"
-              className="font-medium text-blue-400 hover:text-blue-700">
+              href={"/help"}
+              className="font-medium text-blue-300 hover:text-blue-600">
               Help section
             </Link>{" "}
             or{" "}
             <Link
-              href="/contact"
-              className="font-medium text-blue-400 hover:text-blue-700">
-              contact us
+              href={"/contact"}
+              className="font-medium text-blue-300 hover:text-blue-600">
+              contect us
             </Link>
           </p>
         </div>
