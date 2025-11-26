dca1e71ba5c1fe9b1cbabcb19a8fc57e0c590962
Muhammad Faheem <malikfaheem.007001@gmail.com>
2025-11-26 14:15:25 +0500

    some changing

--- components/HomeCategories.tsx
+++ components/HomeCategories.tsx
@@ -29,7 +29,7 @@ const HomeCategories = ({categories}: {categories: Category[]}) => {
             <div className="space-y-1">
               <h3 className="text-base font-semibold">{category?.title}</h3>
               <p className="text-sm">
-                <span className="font-bold text-shop_dark_green">{`(${category?.productCount})`}</span>{" "}
+                <span className="font-bold text-shop_dark_green">{`(${category?.range})`}</span>{" "}
                 items Available
               </p>
             </div>
--- components/ImageView.tsx
+++ components/ImageView.tsx
@@ -1,8 +1,13 @@
+"use client";
 import {
   internalGroqTypeReferenceTo,
   SanityImageCrop,
   SanityImageHotspot,
 } from "@/sanity.types";
+import {urlFor} from "@/sanity/lib/image";
+import {AnimatePresence, motion} from "motion/react";
+import Image from "next/image";
+import {useState} from "react";
 
 interface Props {
   images?: Array<{
@@ -17,11 +22,47 @@ interface Props {
     _type: "image";
     _key: string;
   }>;
-  isStock?: number;
+  isStock?: number | undefined;
 }
 const ImageView = ({images = [], isStock}: Props) => {
+  const [active, setActive] = useState(images[0]);
   return (
-    <div className="w-full md:w-1/2 space-y-2 md:space-y-4 ">ImageView</div>
+    <div className="w-full md:w-1/2 space-y-2 md:space-y-4 ">
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={active?._key}
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          exit={{opacity: 0}}
+          transition={{duration: 0.5}}
+          className="w-full max-h-[550px] min-h-[450px] border border-darkColor/10 rounded-md group overflow-hidden">
+          <Image
+            src={urlFor(active).url()}
+            alt="productImage"
+            width={700}
+            height={700}
+            priority
+            className={`w-full h-96 max-h-[550px] min-h-[500px] object-contain group-hover:scale-110 hoverEffect rounded-md ${isStock === 0 ? "opacity-50" : ""}`}
+          />
+        </motion.div>
+      </AnimatePresence>
+      <div className="grid grid-cols-6 gap-2 h-20 md:h-24">
+        {images.map((image) => (
+          <button
+            key={image?._key}
+            onClick={() => setActive(image)}
+            className={`border border-darkColor/10 rounded-md overflow-hidden ${active?._key === image?._key ? "border-shop_light_green opacity-100" : "opacity-80"}`}>
+            <Image
+              src={urlFor(image).url()}
+              alt={`Thumbnail ${image?._key}`}
+              width={100}
+              height={100}
+              className="w-full h-auto object-contain"
+            />
+          </button>
+        ))}
+      </div>
+    </div>
   );
 };
 
