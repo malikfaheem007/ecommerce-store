9c5726d73c3bf055cb2e7e1bca79a85ca9f4c231
Muhammad Faheem <malikfaheem.007001@gmail.com>
2025-11-26 14:15:08 +0500

    more data add

--- app/(client)/product/[slug]/page.tsx
+++ app/(client)/product/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import Container from "@/components/Container";
 import ImageView from "@/components/ImageView";
 import {getProductBySlug} from "@/sanity/queries";
+import {StarIcon} from "lucide-react";
 
 const SingleProductPage = async ({
   params,
@@ -9,14 +10,30 @@ const SingleProductPage = async ({
 }) => {
   const {slug} = await params;
   const product = await getProductBySlug(slug);
-  //   const isStock = product?.stock > 0;
-  console.log(product);
   return (
-    <Container className="flex flex-col md:flex-row pb-10 gap-10">
-      {/* {product?.images && (
-        // <ImageView images={product?.images} isStock={isStock} />
-      )} */}
-      <div className="w-full md:w-1/2 flex flex-col gap-5 ">details</div>
+    <Container className="flex flex-col md:flex-row py-10 gap-10">
+      {product?.images && (
+        <ImageView images={product?.images} isStock={product?.stock} />
+      )}
+      <div className="w-full md:w-1/2 flex flex-col gap-5 ">
+        <div className="space-y-1">
+          <h2 className="text-2xl font-bold">{product?.name}</h2>
+          <p className="text-sm text-gray-600 tracking-wide">
+            {product?.description}
+          </p>
+        </div>
+        <div className="flex items-center gap-0.5 text-xs">
+          {[...Array(5)].map((_, index) => (
+            <StarIcon
+              key={index}
+              size={12}
+              className="text-shop_light_green"
+              fill={"#3b9c3c"}
+            />
+          ))}
+          <p className="font-semibold">{`(120)`}</p>
+        </div>
+      </div>
     </Container>
   );
 };
