fcf488e1fb182da7c19206e4c577e17cc823f584
Muhammad Faheem <malikfaheem.007001@gmail.com>
2025-11-29 16:15:25 +0500

    new file add

--- components/Shop.tsx
+++ components/Shop.tsx
@@ -1,20 +1,35 @@
 "use client";
-import {BRANDS_QUERYResult, Category} from "@/sanity.types";
+import {BRANDS_QUERYResult, Category, Product} from "@/sanity.types";
 import Container from "./Container";
 import {Title} from "./ui/text";
 import {cn} from "@/lib/utils";
+import CategoryList from "./shop/CategoryList";
+import BrandsList from "./shop/BrandsList";
+import PriceList from "./shop/PriceList";
+import {useSearchParams} from "next/navigation";
+import {useState} from "react";
 
 interface Props {
   categories: Category[];
   brands: BRANDS_QUERYResult;
 }
+
 const Shop = ({categories, brands}: Props) => {
+  const searchParams = useSearchParams();
+  const brandParams = searchParams?.get("brand");
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [selectedBrand, setSelectedBrand] = useState<string | null>(
+    brandParams || null
+  );
+  const [selectedPrice, setSelectedPrice] = useState<string | null>(null);
   return (
     <div className="border-t ">
       <Container>
         <div className="sticky top-0 z-10 mb-5">
           <div className="flex items-center justify-between">
-            <Title className="text-lg uppercase tracking-wide">
+            <Title className="text-base md:text-base uppercase">
               Get the products as your needs
             </Title>
             <button className="text-shop_dark_green underline text-sm mt-2 font-medium hover:text-shop_orange hoverEffect">
@@ -22,7 +37,25 @@ const Shop = ({categories, brands}: Props) => {
             </button>
           </div>
         </div>
-        <div></div>
+        <div className="flex flex-col md:flex-row gap-5 border-t border-t-shop_dark_green/50">
+          <div className="md:sticky md:top-20 md:self-start md:h-[calc(100vh-160px)] md:overflow-y-auto scrollbar-hide md:min-w-64 pb-5 md:border-r border-r-shop_dark_green/50">
+            <CategoryList
+              categories={categories}
+              selectedCategory={selectedCategory}
+              setSelectedCategory={setSelectedCategory}
+            />
+            <BrandsList
+              brands={brands}
+              selectedBrand={selectedBrand}
+              setSelectedBrand={setSelectedBrand}
+            />
+            <PriceList
+              selectedPrice={selectedPrice}
+              setSelectedPrice={setSelectedPrice}
+            />
+          </div>
+          <div>Products</div>
+        </div>
       </Container>
     </div>
   );
