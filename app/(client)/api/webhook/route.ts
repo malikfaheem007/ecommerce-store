b09193f4b58d6cd9139aab892f08989855852dc6
Muhammad Faheem <malikfaheem.007001@gmail.com>
2025-12-22 17:13:51 +0500

    payment  done

--- app/(client)/api/webhook/route.ts
+++ app/(client)/api/webhook/route.ts
@@ -43,63 +43,43 @@ export async function POST(req: NextRequest) {
       session.invoice ?
         await stripe.invoices.retrieve(session.invoice as string)
       : null;
-    console.log("console 14");
-
     try {
-      console.log("console 15");
-
       await createOrderInSanity(session, invoice);
     } catch (error) {
-      console.log("console 16");
-
       console.error("Error Creating order in sanity:", error);
       return NextResponse.json(
         {error: `Error creating order : ${error}`},
         {status: 400}
       );
     }
-    console.log("console 17");
-
     return NextResponse.json({received: true}, {status: 200});
   }
 }
-console.log("console 18");
-
 async function createOrderInSanity(
   session: Stripe.Checkout.Session,
   invoice: Stripe.Invoice | null
 ) {
-  console.log("console 19");
-
   const {id, amount_total, currency, metadata, payment_intent, total_details} =
     session;
-  console.log("console 20");
 
   const {orderNumber, customerName, customerEmail, clerkUserId, address} =
     metadata as unknown as Metadata & {address: string};
-  console.log("console 21");
 
   const parsedAddress = address ? JSON.parse(address) : null;
   const lineItemsWithProduct = await stripe.checkout.sessions.listLineItems(
     id,
     {expand: ["data.price.product"]}
   );
-  console.log("console 22");
 
   //Create Sanity product references and prepare stock updates
   const sanityProducts = [];
   const stockUpdates = [];
-  console.log("console 23");
 
   for (const item of lineItemsWithProduct.data) {
-    console.log("console 24");
-
     const productId = (item.price?.product as Stripe.Product)?.metadata?.id;
     const quantity = item.quantity || 0;
-    console.log("console 25");
 
     if (!productId) continue;
-    console.log("console 26");
 
     sanityProducts.push({
       _key: crypto.randomUUID(),
@@ -109,10 +89,8 @@ async function createOrderInSanity(
       },
       quantity,
     });
-    console.log("console 27");
 
     stockUpdates.push({productId, quantity});
-    console.log("console 28");
   }
   //Create order in Sanity
   const order = await backendClient.create({
@@ -150,7 +128,6 @@ async function createOrderInSanity(
         }
       : null,
   });
-  console.log("console 29");
 
   // Update stock levels in Sanity
   await updateStockLevels(stockUpdates);
@@ -163,37 +140,25 @@ async function createOrderInSanity(
 async function updateStockLevels(
   stockUpdates: {productId: string; quantity: number}[]
 ) {
-  console.log("console 31");
-
   for (const {productId, quantity} of stockUpdates) {
     try {
-      console.log("console 32");
-
       // Fetch current stock
       const product = await backendClient.getDocument(productId);
-      console.log("console 33");
 
       if (!product || typeof product.stock !== "number") {
         console.warn(
           `Product with ID ${productId} not found or stock is invalid.`
         );
-        console.log("console 34");
 
         continue;
       }
-      console.log("console 35");
 
       const newStock = Math.max(product.stock - quantity, 0); //Ensure stock does not go negative
-      console.log("console 36");
 
       // Update stock in Sanity
       await backendClient.patch(productId).set({stock: newStock}).commit();
-      console.log("console 37");
     } catch (error) {
-      console.log("console 38");
-
       console.error(`Failed to update stock for product  ${productId}:`, error);
     }
   }
-  console.log("console 39");
 }
